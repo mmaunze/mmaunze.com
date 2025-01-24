0230472d703611aaf63029f6d4921372eb711a98
mmaunze <mlmaunze@gmail.com>
2025-01-24 22:41:44 +0200

    update 404

--- pages/error/404.php
+++ pages/error/404.php
@@ -8,108 +8,76 @@
   data-assets-path="../../assets/"
   data-template="horizontal-menu-template"
   data-style="light">
-  <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Error | Meldo Maunze - Engenheiro Informático </title>
+<head>
+  <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <meta name="description" content="" />
+  <title>Error | Meldo Maunze - Engenheiro Informático </title>
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
+  <meta name="description" content="" />
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap"
-      rel="stylesheet" />
 
-    <!-- Icons -->
-    <link rel="stylesheet" href="../../assets/vendor/fonts/remixicon/remixicon.css" />
-    <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
+  <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="../../assets/vendor/fonts/remixicon/remixicon.css" />
+  <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
+  <link rel="stylesheet" href="../../assets/vendor/libs/node-waves/node-waves.css" />
+  <link rel="stylesheet" href="../../assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
+  <link rel="stylesheet" href="../../assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
+  <link rel="stylesheet" href="../../assets/css/demo.css" />
+  <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+  <link rel="stylesheet" href="../../assets/vendor/libs/typeahead-js/typeahead.css" />
+  <link rel="stylesheet" href="../../assets/vendor/css/pages/page-misc.css" />
+  <script src="../../assets/vendor/js/helpers.js"></script>
+  <script src="../../assets/vendor/js/template-customizer.js"></script>
+  <script src="../../assets/js/config.js"></script>
+</head>
 
-    <!-- Menu waves for no-customizer fix -->
-    <link rel="stylesheet" href="../../assets/vendor/libs/node-waves/node-waves.css" />
+<body>
 
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="../../assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="../../assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="../../assets/css/demo.css" />
 
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-    <link rel="stylesheet" href="../../assets/vendor/libs/typeahead-js/typeahead.css" />
+  <div class="misc-wrapper">
+    <h1 class="mb-2 mx-2" style="font-size: 6rem; line-height: 6rem">404</h1>
+    <h4 class="mb-2">Página Não Encontrada ⚠️</h4>
 
-    <!-- Page CSS -->
-    <!-- Page -->
-    <link rel="stylesheet" href="../../assets/vendor/css/pages/page-misc.css" />
+    <div class="d-flex justify-content-center mt-7">
+      <img
+        src="../../assets/img/illustrations/misc-error-object.png"
+        alt="misc-error"
+        class="img-fluid misc-object d-none d-lg-inline-block"
+        width="160" />
+      <img
+        src="../../assets/img/illustrations/misc-bg-light.png"
+        alt="misc-error"
+        class="misc-bg d-none d-lg-inline-block"
+        data-app-light-img="illustrations/misc-bg-light.png"
+        data-app-dark-img="illustrations/misc-bg-dark.png" />
+      <div class="d-flex flex-column align-items-center">
 
-    <!-- Helpers -->
-    <script src="../../assets/vendor/js/helpers.js"></script>
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-    <script src="../../assets/vendor/js/template-customizer.js"></script>
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="../../assets/js/config.js"></script>
-  </head>
-
-  <body>
-    <!-- Content -->
-
-    <!-- Error -->
-    <div class="misc-wrapper">
-      <h1 class="mb-2 mx-2" style="font-size: 6rem; line-height: 6rem">404</h1>
-      <h4 class="mb-2">Page Not Found ⚠️</h4>
-   
-      <div class="d-flex justify-content-center mt-7">
-        <img
-          src="../../assets/img/illustrations/misc-error-object.png"
-          alt="misc-error"
-          class="img-fluid misc-object d-none d-lg-inline-block"
-          width="160" />
-        <img
-          src="../../assets/img/illustrations/misc-bg-light.png"
-          alt="misc-error"
-          class="misc-bg d-none d-lg-inline-block"
-          data-app-light-img="illustrations/misc-bg-light.png"
-          data-app-dark-img="illustrations/misc-bg-dark.png" />
-        <div class="d-flex flex-column align-items-center">
-          
-            <p class="mb-4 mx-2">we couldn't find the page you are looking for</p>
-            <div>
-         
-            <a href="/" class="btn btn-info text-center my-10">Voltar ao Inicio</a>
-          </div>
+        <p class="mb-4 mx-2">Não conseguimos encontrar a página que você está procurando</p>
+        <div>
+          <a href="/" class="btn btn-info text-center my-10">Voltar ao Início</a>
         </div>
       </div>
     </div>
-    <!-- /Error -->
 
-    <!-- / Content -->
+  </div>
 
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
-    <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="../../assets/vendor/libs/popper/popper.js"></script>
-    <script src="../../assets/vendor/js/bootstrap.js"></script>
-    <script src="../../assets/vendor/libs/node-waves/node-waves.js"></script>
-    <script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="../../assets/vendor/libs/hammer/hammer.js"></script>
-    <script src="../../assets/vendor/libs/i18n/i18n.js"></script>
-    <script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
-    <script src="../../assets/vendor/js/menu.js"></script>
+  <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
+  <script src="../../assets/vendor/libs/popper/popper.js"></script>
+  <script src="../../assets/vendor/js/bootstrap.js"></script>
+  <script src="../../assets/vendor/libs/node-waves/node-waves.js"></script>
+  <script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+  <script src="../../assets/vendor/libs/hammer/hammer.js"></script>
+  <script src="../../assets/vendor/libs/i18n/i18n.js"></script>
+  <script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
+  <script src="../../assets/vendor/js/menu.js"></script>
+  <script src="../../assets/js/main.js"></script>
+</body>
 
-    <!-- endbuild -->
-
-    <!-- Vendors JS -->
-
-    <!-- Main JS -->
-    <script src="../../assets/js/main.js"></script>
-
-    <!-- Page JS -->
-  </body>
 </html>
