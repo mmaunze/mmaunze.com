d60f3e1703e79e26a590567cd83d3b71f879bb39
Meldo Maunze <114779063+mmaunze@users.noreply.github.com>
2025-12-14 00:22:16 +0200

    Atualizar o login.php

--- private/login.php
+++ private/login.php
@@ -15,7 +15,7 @@
     name="viewport"
     content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-  <title>Login Cover - Pages | Materialize - Material Design HTML Admin Template</title>
+  <title>Login</title>
 
   <meta name="description" content="" />
 
