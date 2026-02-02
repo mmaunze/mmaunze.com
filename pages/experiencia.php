38867bcda1b1d74e60d8623519fe496506bb7744
Meldo Maunze <114779063+mmaunze@users.noreply.github.com>
2026-02-02 16:45:16 +0200

    Update experiencia.php

--- pages/experiencia.php
+++ pages/experiencia.php
@@ -103,7 +103,10 @@ get_part('header');
                                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
                             </svg>
                             <span>Responsável pela inovação contínua: adopção de novas tecnologias, modernização de software e formação interna/externa em ferramentas de produtividade e cibersegurança.</span>
+                            <span>Website: <a href="https://mefemasys.co.mz" target="_blank" rel="noopener noreferrer" class="text-brand-blue hover:underline font-medium">mefemasys.co.mz</a></span>
+
                         </li>
+                        
                     </ul>
                 </div>
             </div>
